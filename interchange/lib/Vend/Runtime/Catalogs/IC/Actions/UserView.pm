7c883e1eb528afdbe5b75e334fe372430e5c8373
Terry Grant <terry@endpoint.com>
2008-09-18 01:15:42 -0400

    Initial commit

---
+++ interchange/lib/Vend/Runtime/Catalogs/IC/Actions/UserView.pm
@@ -0,0 +1,26 @@
+package Vend::Runtime::Catalogs::IC::Actions::UserView;
+use Moose;
+
+has 'user_id' => (is  => 'rw', 
+                  isa => 'Int');
+
+has 'view_all' => (is  => 'rw', 
+                   isa => 'Int');
+
+1;
+
+=pod
+
+=head1 NAME 
+
+Vend::Runtime::Catalogs::IC::Actions::Userview  
+
+=head1 SYSOPSIS
+
+=head1 DESCRIPTION 
+
+=head1 PARAMETERS 
+
+=head1 METHODS
+
+=back
---
+++ interchange/lib/Vend/Runtime/Global/Actions/ProductView.pm
@@ -0,0 +1,23 @@
+package Vend::Runtime::Global::Actions::ProductView;
+use Moose;
+
+has 'product_id' => (is  => 'rw', 
+                     isa => 'Int');
+
+1;
+
+=pod
+
+=head1 NAME 
+
+Vend::Runtime::Global::Actions::ProductView;
+
+=head1 SYSOPSIS
+
+=head1 DESCRIPTION 
+
+=head1 PARAMETERS 
+
+=head1 METHODS
+
+=back
