d46922dda2ee69a35cbf966a180c38cba810909a
Terry Grant <terry@endpoint.com>
2009-02-05 02:24:58 -0500

    Added first rough draft of SYNOPSIS to the POD

--- interchange/lib/Vend/Action/Standard.pm
+++ interchange/lib/Vend/Action/Standard.pm
@@ -6,9 +6,35 @@ sub { return 1; }
 
 =head1 NAME 
 
-Vend::Action::Standard - a class that represents 
+Vend::Action::Standard - a class that represents an ActionMap directive 
 
 =head1 SYSOPSIS
+
+use Vend::Action;
+use Vend::Action::Standard;
+
+# These values will usually come from the ActionMap namespace
+my $name = 'product_detail';
+
+my $sub = sub { 
+	$CGI->{mv_nextpage} = 'product_detail.html'; 
+	return 1; 
+};
+
+my $action = Vend::Action::Standard->new($name, $sub);
+
+my $name = $action->name();
+my $sub = $action->sub();
+
+# Have Vend::Action::do_action() execute the sub 
+$status = Vend::Action::do_action(
+	$action->name(), 
+	$action->sub(), 
+	\@path
+);
+
+# NOTE: maybe do_action should be in this class as it is executing these subs
+$status = $action->do_action();  # this would simply use the instances attributes to execute itself...not in a lethal way as suicide is frowned upon
 
 =head1 DESCRIPTION 
 
